6df5eeb433cd8342082d2096ad19e61edbb3e06c
lmcassidy <lauren.cassidy.nd@gmail.com>
2026-02-03 20:32:40 +0000

    Remove unused import of Optional from price_predictor.py

--- src/sustainability_scoring/models/price_predictor.py
+++ src/sustainability_scoring/models/price_predictor.py
@@ -1,7 +1,5 @@
 """Price prediction models using sustainability features."""
 
-from typing import Optional
-
 import numpy as np
 import pandas as pd
 from sklearn.ensemble import RandomForestRegressor, GradientBoostingRegressor
